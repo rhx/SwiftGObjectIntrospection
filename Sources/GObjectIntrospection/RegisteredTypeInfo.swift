59cf9efd7d55170ea14b5ef43da10e48454b8a5f
Rene Hexel <rhx@users.noreply.github.com>
2021-12-20 19:00:53 +1000

    Add RegisteredTypeInfo

---
+++ Sources/GObjectIntrospection/RegisteredTypeInfo.swift
@@ -0,0 +1,17 @@
+//
+//  RegisteredTypeInfo.swift
+//  
+//
+//  Created by Rene Hexel on 20/12/2021.
+//
+import CGObjectIntrospection
+
+public class RegisteredTypeInfo: BaseInfo {
+    /// Return the type name of the registered type
+    @inlinable public var registerdTypeName: String { String(cString: g_registered_type_info_get_type_name(baseInfo)) }
+    /// Return the type initialization function name of the registered type
+    @inlinable public var typeInit: String { String(cString: g_registered_type_info_get_type_init(baseInfo)) }
+    /// Return the type of the registered type
+    @inlinable public var registeredType: GType { g_registered_type_info_get_g_type(baseInfo) }
+}
+
